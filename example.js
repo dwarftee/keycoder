0fdd734f87e4f07691ef7c1faea29f92a1ae3d40
sharpi <109460706+sharpi1@users.noreply.github.com>
2023-01-07 08:06:18 +0000

    commit :0

---
+++ example.js
@@ -0,0 +1,32 @@
+//this is a example of use
+
+const a = "æ";
+const b = "”";
+const c = "¢";
+const d = "ð";
+const e = "¹";
+const f = "đ";
+const g = "ŋ";
+const h = "ħ";
+const i = "→";
+const j = "²";
+const k = "ĸ";
+const l = "³";
+const m = "µ";
+const n = "€";
+const o = "ø";
+const p = "þ";
+const q = "@";
+const r = "¶";
+const s = "ß";
+const t = "ŧ";
+const u = "↓";
+const v = "“";
+const w = "ł";
+const x = "»";
+const y = "←";
+const z = "«";
+
+
+const hello = h+e+l+l+o;
+console.log(hello); //output: ħ¹³³ø
